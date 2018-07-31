5f1a011c8b960e8f6b0c728e3598d38039b68666
Maverick Tesini <mavericktesini@gmail.com>
2018-07-31 00:12:13 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,8 @@ class Post < ActiveRecord::Base
   belongs_to :author
   validate :is_title_case
 
-  # before_save :make_title_case
-  before_validation :make_title_case
+  # before_save :make_title_case #occurs just before save to db.
+  before_validation :make_title_case #occurs before BOTH validation and save to db.
   before_save :email_author_about_post #author is alerted after validation and just prior to db alteration.
   private
 
